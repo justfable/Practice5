b98b92f1b37a9cb48dbfab8dde1302e55f373e42
Mukhammedali Zhursinbek <zhursinbekali@mail.ru>
2023-10-13 18:24:26 +0600

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                MethodA();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Исключение перехвачено в Main: {ex.Message}");
+            }
+        }
+
+        static void MethodA()
+        {
+            try
+            {
+                MethodB();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Исключение перехвачено в MethodA: {ex.Message}");
+                throw;
+            }
+        }
+
+        static void MethodB()
+        {
+            throw new InvalidOperationException("Произошла ошибка в MethodB.");
+        }
+    }
+}
